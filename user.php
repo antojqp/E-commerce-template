c6da4f10f52b0903cbab0d2966fe9aa4345817e4
god <antoniojoseqp@hotmail.com>
2019-04-06 02:29:11 -0300

    Update user.php
    added first rules to the user page things like not being able to enter without being logged

--- user.php
+++ user.php
@@ -3,6 +3,24 @@
 	if (!isset($_SESSION['user'])) {
 		#this will check if the user is logged and got here by legitimate manners
 
-		sda
+		header("Location: index.php?error=notlogged");
+		exit();
 	}
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profile <?php echo $_SESSION['user']; ?></title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/uikit.css">
+    <script src="js/uikit.js"></script>
+    <script src="js/uikit-icons.js"></script>
+</head>
+<body>
+<?php include 'header.php'; ?>
+
+
+<?php include 'footer.php'; ?>
+</body>
+</html>
